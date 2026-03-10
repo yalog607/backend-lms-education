16933fdf35b8145975b2b5ca48033186f7d42bf3
Yalog <nguyenyalog@gmail.com>
2026-03-10 08:40:53 +0700

    fix req body comment API

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -4,10 +4,10 @@ export const getComments = async(req, res) => {
     try {
         const { lessonId } = req.params;
 
-        const comments = await Comment.find({ lesson: lessonId })
-            .populate("user", "fName lName avatar") // Chỉ lấy các trường cần thiết của User
-            .sort({ createdAt: -1 }); // Bình luận mới nhất lên đầu
-
+        const comments = await Comment.find({ lesson_id: lessonId })
+            .populate("user_id", "first_name last_name avatar") 
+            .sort({ createdAt: -1 });
+            
         res.status(200).json({
             success: true,
             count: comments.length,
@@ -20,28 +20,28 @@ export const getComments = async(req, res) => {
 
 export const createComment = async(req, res) => {
 try {
-        const { content, lessonId } = req.body;
+        const { content, lesson_id } = req.body;
         const userId = req.userId;
 
         if (!content) {
             return res.status(400).json({ message: "Nội dung bình luận không được để trống" });
         }
 
-        const newComment = await Comment.create({
+        const newComment = await new Comment({
             content,
-            user: userId,
-            lesson: lessonId
+            lesson_id,
+            user_id: userId,
         });
 
-        const populatedComment = await newComment.populate("user", "fName lName avatar");
+        await newComment.save();
 
         res.status(201).json({
             success: true,
-            message: "Đã gửi bình luận",
-            data: populatedComment
+            message: "Post a comment successfully!"
         });
     } catch (error) {
         res.status(500).json({ message: "Lỗi khi gửi bình luận", error: error.message });
+        console.error("Lỗi khi gửi bình luận:", error);
     }
 }
 
@@ -56,7 +56,7 @@ export const deleteComment = async(req, res) => {
             return res.status(404).json({ message: "Bình luận không tồn tại" });
         }
 
-        if (comment.user.toString() !== userId) {
+        if (comment.user_id.toString() !== userId) {
             return res.status(403).json({ message: "Bạn không có quyền xóa bình luận này" });
         }
 
